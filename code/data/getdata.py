e4bda49fc94254029b5a85831ade2b998be8a4b8
Dylan <dylanjahansen@gmail.com>
2022-11-10 18:29:52 -0700

    Added template

---
+++ code/data/getdata.py
@@ -0,0 +1,9 @@
+import requests
+import json
+import secrets
+
+CLIENT_ID = "bf095f8cb3a46df427e555beb47b1259"
+CLIENT_SECRET = "06bce3e7e9711441b96a65a30b6bccf0bc9831fbb5e1c8948a8ea648eca79d8f"
+
+if __name__ == "__main__":
+    CLIENT_ID
