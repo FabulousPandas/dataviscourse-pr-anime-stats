91b1a65b2d44d6bc13e3cbbaa81a9fca85c0c605
Khris Thammavong <khristha@gmail.com>
2022-11-11 17:07:20 -0700

    loading from the new data files, currently using the small one for testing

--- code/js/project.js
+++ code/js/project.js
@@ -1,12 +1,14 @@
 // Data Loading
 async function loadData() {
-    const animeData = await d3.json('data/template.json')
-    return animeData
+    const seasonData = await d3.json('data/small_anime_seasons.json')
+    const genreData = await d3.json('data/small_anime_genres.json')
+    return [seasonData, genreData]
 }
 
 // Data used throughout our application
 const globalApplicationState =  {
-    animeData: null,
+    seasonData: null,
+    genreData: null,
     genres: ["Action", "Adventure", "Avant Garde", "Boys Love", "Comedy", "Drama", 
              "Fantasy", "Girls Love", "Gourmet", "Horror", "Mystery", "Romance", 
              "Sci-Fi", "Slice of Life", "Sports", "Supernatural", "Suspense"],
@@ -15,7 +17,9 @@ const globalApplicationState =  {
 
 // Application Mounting
 loadData().then((loadedData => { 
-    globalApplicationState.animeData = loadedData
+    const [seasonData, genreData] = loadedData
+    globalApplicationState.seasonData = seasonData
+    globalApplicationState.genreData = genreData
 
     lineChart = LineChart(globalApplicationState)
     
