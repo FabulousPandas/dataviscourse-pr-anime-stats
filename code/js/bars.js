6acee832a0b4a745a9c69766da7e40f1fd0c9e51
Dylan <dylanjahansen@gmail.com>
2022-11-11 23:02:53 -0700

    Width fixed

--- code/js/bars.js
+++ code/js/bars.js
@@ -27,6 +27,7 @@ class BarGraph {
         this.scaleY = d3.scaleLinear()
             .domain([0, d3.max(this.genreData, (d) => d[1].length)])
             .range([this.visHeight, 0])
+        this.colorScale = d3.scaleOrdinal().domain(this.globalState.selectedGenres).range(d3.schemeCategory10)
         this.drawAxes()
         this.drawBars()
     }
@@ -46,7 +47,8 @@ class BarGraph {
             .join("rect")
                 .attr("x", (d) => this.scaleX(d[0]) + this.marginX)
                 .attr("y", (d) => this.scaleY(d[1].length))
-                .attr("width", 50)
+                .attr("width", this.scaleX.bandwidth() - 100)
                 .attr("height", (d) => this.visHeight - this.scaleY(d[1].length) - this.marginY)
+                .attr("fill", (d) => this.colorScale(d[0]))
     }
 }
